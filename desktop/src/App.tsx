4208209f0aec9b8ea9d53f275f41fbac7d5ddab3
jaylfc <jaylfc25@gmail.com>
2026-04-09 20:56:22 +0100

    scaffold desktop shell: Vite + React 19 + TypeScript + Tailwind

---
+++ desktop/src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div style={{ height: "100vh", width: "100vw", background: "#1a1b2e", color: "rgba(255,255,255,0.5)", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      TinyAgentOS Desktop Shell
+    </div>
+  );
+}
